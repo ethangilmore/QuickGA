7b7fc451d2e6d5ddc4fb02c4d20567b176badf9c
ethangilmore <ethanmckgilmore@gmail.com>
2022-10-16 21:17:11 +0900

    Added mutation functions for sequence trait

--- quickga/traits/__init__.py
+++ quickga/traits/__init__.py
@@ -1,3 +1,4 @@
 from .basetrait import BaseTrait
+from .sequencetrait import SequenceTrait
 from .inttrait import IntTrait
 from .floattrait import FloatTrait
--- quickga/traits/sequencetrait.py
+++ quickga/traits/sequencetrait.py
@@ -3,6 +3,12 @@ from .basetrait import BaseTrait
 
 class SequenceTrait(BaseTrait):
 
+    def __random_unique_index_pair(self, items):
+        index_a = random.randint(0,len(items)-1)
+        index_b = random.randint(0,len(items)-2)
+        index_b += 1 if index_b >= index_a else 0
+        return index_a, index_b
+
     def uniform_crossover(self, a, b):
         return [a[i] if random.random()<.5 else b[i] for i in range(len(a))]
 
@@ -18,13 +24,13 @@ class SequenceTrait(BaseTrait):
         if not self.n:
             raise Exception("No n defined for n-point crossover")
         
-        indices = [i for i in range(len(a))]
+        indices = [i for i in range(1, len(a)-1)]
         cross_indices = [indices.pop(random.randint(0,len(indices)-1)) for i in range(self.n)]
         
         new_sequence = []
         pick_from_a = random.random()<.5
         for i in range(len(a)):
-            new_sequence += a[i] if pick_from_a else b[i]
+            new_sequence.append(a[i] if pick_from_a else b[i])
             if i in cross_indices:
                 pick_from_a = not pick_from_a
 
@@ -32,22 +38,45 @@ class SequenceTrait(BaseTrait):
 
 
     def random_reset_mutation(self, value):
-        pass
+        random_index = random.randint(0,len(value)-1)
+        value[random_index] = self.trait.random_value()
 
-    def swap_mutation(self, value):
-        pass
+        return value
 
     def insertion_mutation(self, value):
-        pass
+        remove_index, insert_index = self.__random_unique_index_pair(value)
+        temp = value.pop(remove_index)
+        value.insert(insert_index, temp)
+
+        return value
+
+    def swap_mutation(self, value):
+        index_a, index_b = self.__random_unique_index_pair(value)
+
+        temp = value[index_a]
+        value[index_a] = value[index_b]
+        value[index_b] = temp
+
+        return value
 
     def scramble_mutation(self, value):
-        pass
+        index_a, index_b = self.__random_unique_index_pair(value)
+        scramble_section = value[index_a:index_b]
+        random.shuffle(scramble_section)
+        value[index_a:index_b] = scramble_section
+
+        return value
 
     def inversion_mutation(self, value):
-        pass
+        index_a, index_b = self.__random_unique_index_pair(value)
+        invert_section = value[index_a:index_b]
+        invert_section.reverse()
+        value[index_a:index_b] = invert_section
+
+        return value
 
 
-    def __init__(self, trait, length: int, crossover_type: str, mutation_type: str, mutation_rate: float, n: int=None):
+    def __init__(self, trait, length: int, crossover_type: str, mutation_type: str, mutation_rate: float=0.05, n: int=None):
         self.trait = trait
         self.length = length
         self.crossover_type = crossover_type
